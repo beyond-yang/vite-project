198e2d58673a0e0915ddc682f1075193f828fc7a
杨静 <1369374442@qq.com>
2023-02-19 20:10:55 +0800

    feat: 抽离出header组件和左侧菜单组件

--- src/components/container/src/index.vue
+++ src/components/container/src/index.vue
@@ -1,52 +1,11 @@
 <template>
   <el-container>
       <el-aside width="auto">
-        <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          :collapse="isCollapse">
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon>
-                <location />
-              </el-icon>
-              <span>Navigator One</span>
-            </template>
-            <el-menu-item-group>
-              <template #title><span>Group One</span></template>
-              <el-menu-item index="1-1">item one</el-menu-item>
-              <el-menu-item index="1-2">item two</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group Two">
-              <el-menu-item index="1-3">item three</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="1-4">
-              <template #title><span>item four</span></template>
-              <el-menu-item index="1-4-1">item one</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-menu-item index="2">
-            <el-icon><icon-menu /></el-icon>
-            <template #title>Navigator Two</template>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <el-icon>
-              <document />
-            </el-icon>
-            <template #title>Navigator Three</template>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <el-icon>
-              <setting />
-            </el-icon>
-            <template #title>Navigator Four</template>
-          </el-menu-item>
-        </el-menu>
+        <navAside :collapse="isCollapse"></navAside>
       </el-aside>
       <el-container>
         <el-header>
-          <el-icon v-if="isCollapse" style="cursor: pointer;" @click="isCollapse = !isCollapse"><Expand /></el-icon>
-          <el-icon v-if="!isCollapse" style="cursor: pointer;" @click="isCollapse = !isCollapse"><Fold /></el-icon>
+          <navHeader v-model:collapse="isCollapse"></navHeader>
         </el-header>
         <el-main><el-icon><Plus /></el-icon></el-main>
       </el-container>
@@ -55,6 +14,8 @@
 
 <script setup lang='ts'>
 import { ref } from 'vue';
+import navAside from '@/components/container/src/navSide/index.vue';
+import navHeader from '@/components/container/src/navHeader/index.vue';
 const isCollapse = ref(false);
 
 </script>
@@ -62,17 +23,8 @@ const isCollapse = ref(false);
 .el-container {
   height: 100%;
 }
-.el-menu {
-  background-color: #d9ecff;
-}
-.el-header {
-  background-color: #c6e2ff;
-}
-.el-main {
-  background-color: #ecf5ff;
-}
 
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
+.el-main {
+  // background-color: #ecf5ff;
 }
 </style>
---
+++ src/components/container/src/navHeader/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header-container">
+    <span style="cursor: pointer;" @click="toggleStatus">
+      <el-icon v-if="collapse">
+        <Expand />
+      </el-icon>
+      <el-icon v-else>
+        <Fold />
+      </el-icon>
+    </span>
+  </div>
+</template>
+
+<script setup lang='ts'>
+let props = defineProps<{
+  collapse: boolean
+}>();
+let emits = defineEmits(['update:collapse']);
+
+const toggleStatus = () => {
+  emits('update:collapse', !props.collapse)
+};
+</script>
+
+<style lang='scss' scoped>
+.header-container {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  border-bottom: 1px solid #ccc;
+}
+</style>
